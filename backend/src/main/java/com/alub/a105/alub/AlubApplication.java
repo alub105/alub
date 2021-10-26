8bbb0db7acd863ff928247a29ff6e8cbdc54042a
hoonti06 <hoonti06@gmail.com>
2021-10-26 14:04:42 +0900

    feat : 프로젝트 생성 및 DB 구축 (#2)
    close #S05P31A105-42

---
+++ backend/src/main/java/com/alub/a105/alub/AlubApplication.java
@@ -0,0 +1,13 @@
+package com.alub.a105.alub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlubApplication.class, args);
+    }
+
+}
