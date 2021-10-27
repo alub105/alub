e192c346a994f776e56919a0eebb2bc201a3689b
hoonti06 <hoonti06@gmail.com>
2021-10-27 11:21:54 +0900

    fix : UserController API 요청 url 수정

--- backend/src/main/java/com/a105/alub/api/controller/UserController.java
+++ backend/src/main/java/com/a105/alub/api/controller/UserController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequestMapping("/api/users")
+@RequestMapping("/api/user")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
