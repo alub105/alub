30b4bdb5ced362f04b2cd52bd361886132b10f8f
adlguswjda <adlguswjda@naver.com>
2021-10-31 12:11:29 +0900

    chore: check style 적용

--- backend/src/main/java/com/a105/alub/api/service/UserService.java
+++ backend/src/main/java/com/a105/alub/api/service/UserService.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import com.a105.alub.api.request.LoginReq;
 import com.a105.alub.api.response.LoginRes;
 
-public interface UserService extends UserDetailsService{
+public interface UserService extends UserDetailsService {
   
   LoginRes login(LoginReq loginReq);
 }
