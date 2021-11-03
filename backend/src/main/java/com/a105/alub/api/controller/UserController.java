205fe6ce9f740a969d55bfb2d88cea2c01b8f8f4
hoonti06 <hoonti06@gmail.com>
2021-11-03 15:49:26 +0900

    feat : repo 설정 관련 REST API 추가 (#17)
    - github public repo 목록 조회 기능
    - alub repo 설정 기능
    - 특정 github repo 조회 기능
    
    closes S05P31A105-60

--- backend/src/main/java/com/a105/alub/api/controller/UserController.java
+++ backend/src/main/java/com/a105/alub/api/controller/UserController.java
@@ -1,15 +1,24 @@
 package com.a105.alub.api.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import com.a105.alub.api.request.LoginReq;
+import com.a105.alub.api.request.RepoSetReq;
+import com.a105.alub.api.response.GithubRepoRes;
 import com.a105.alub.api.response.LoginRes;
 import com.a105.alub.api.service.UserService;
 import com.a105.alub.common.response.ApiResponseDto;
+import com.a105.alub.security.CurrentUser;
+import com.a105.alub.security.UserPrincipal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Slf4j
 @RequestMapping("/api/user")
@@ -18,14 +27,44 @@ import lombok.extern.slf4j.Slf4j;
 public class UserController {
 
   private final UserService userService;
-  
+
   @PostMapping("/authenticate")
-  public ApiResponseDto<LoginRes> login(@RequestBody LoginReq loginReq){
-    
+  public ApiResponseDto<LoginRes> login(@RequestBody LoginReq loginReq) {
     LoginRes loginResponse = userService.login(loginReq);
     log.info("Login Response: {}", loginResponse);
-    
+
     return ApiResponseDto.success(loginResponse);
   }
-  
+
+  @GetMapping("/repos")
+  public ApiResponseDto<List<GithubRepoRes>> getGithubPublicRepos(
+      @ApiIgnore @CurrentUser UserPrincipal userPrincipal) {
+
+    List<GithubRepoRes> githubRepoResList = userService.getGithubPublicRepos(userPrincipal.getId());
+    log.info("GET /repos - response : {}, userId : {}", githubRepoResList.toArray(),
+        userPrincipal.getId());
+    return ApiResponseDto.success(githubRepoResList);
+  }
+
+  @PutMapping("/repos")
+  public ApiResponseDto<String> setAlubRepo(@RequestBody RepoSetReq repoSetReq,
+      @ApiIgnore @CurrentUser UserPrincipal userPrincipal) {
+    log.info("PUT /repos - request : {}, userId : {}", repoSetReq.toString(),
+        userPrincipal.getId());
+
+    userService.setAlubRepo(userPrincipal.getId(), repoSetReq);
+    return ApiResponseDto.DEFAULT_SUCCESS;
+  }
+
+  @GetMapping("/repos/{repoName}")
+  public ApiResponseDto<GithubRepoRes> getGithubRepo(@PathVariable String repoName,
+      @ApiIgnore @CurrentUser UserPrincipal userPrincipal) {
+    log.info("GET /repos/{} - userId : {}", repoName, userPrincipal.getId());
+
+    GithubRepoRes githubRepoRes = userService.getGithubRepo(userPrincipal.getId(), repoName);
+    log.info("GET /repos/{} - userId : {}, response : {}", repoName, userPrincipal.getId(),
+        githubRepoRes.toString());
+    return ApiResponseDto.success(githubRepoRes);
+  }
+
 }
--- backend/src/main/java/com/a105/alub/api/service/UserService.java
+++ backend/src/main/java/com/a105/alub/api/service/UserService.java
@@ -1,17 +1,28 @@
 package com.a105.alub.api.service;
 
+import com.a105.alub.api.request.LoginReq;
+import com.a105.alub.api.request.RepoSetReq;
+import com.a105.alub.api.response.GithubRepoRes;
+import com.a105.alub.api.response.LoginRes;
+import com.a105.alub.domain.enums.Platform;
+import com.a105.alub.security.UserPrincipal;
+import java.util.List;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import com.a105.alub.api.request.LoginReq;
-import com.a105.alub.api.response.LoginRes;
-import com.a105.alub.domain.enums.Platform;
 
 public interface UserService extends UserDetailsService {
   
   LoginRes login(LoginReq loginReq);
 
+  UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
   UserDetails loadUserByUsername(String username, Platform platform)
       throws UsernameNotFoundException;
-  
+
+  List<GithubRepoRes> getGithubPublicRepos(Long userId);
+
+  GithubRepoRes getGithubRepo(Long userId, String repoName);
+
+  void setAlubRepo(Long userId, RepoSetReq repoSetReq);
 }
