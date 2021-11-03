24081131afe11c5b5be9c736ce08dc83d5dba6e6
yonghun <greenboy94@naver.com>
2021-11-03 15:40:22 +0900

    S05P31A105-64 유저 정보 조회 (#15)
    * feat: get user info
    
    Closes S05P31A105-64

--- backend/src/main/java/com/a105/alub/api/controller/UserController.java
+++ backend/src/main/java/com/a105/alub/api/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.a105.alub.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.a105.alub.api.request.LoginReq;
 import com.a105.alub.api.response.LoginRes;
+import com.a105.alub.api.response.MyInfoRes;
 import com.a105.alub.api.service.UserService;
 import com.a105.alub.common.response.ApiResponseDto;
+import com.a105.alub.security.CurrentUser;
+import com.a105.alub.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Slf4j
 @RequestMapping("/api/user")
@@ -18,14 +23,22 @@ import lombok.extern.slf4j.Slf4j;
 public class UserController {
 
   private final UserService userService;
-  
+
+  @GetMapping("/")
+  public ApiResponseDto<MyInfoRes> getMyInfo(@ApiIgnore @CurrentUser UserPrincipal userPrincipal) {
+    MyInfoRes userInfoRes = userService.getMyInfo(userPrincipal.getId());
+    log.info("Get User Info: {}", userInfoRes);
+
+    return ApiResponseDto.success(userInfoRes);
+  }
+
   @PostMapping("/authenticate")
-  public ApiResponseDto<LoginRes> login(@RequestBody LoginReq loginReq){
-    
+  public ApiResponseDto<LoginRes> login(@RequestBody LoginReq loginReq) {
+
     LoginRes loginResponse = userService.login(loginReq);
     log.info("Login Response: {}", loginResponse);
-    
+
     return ApiResponseDto.success(loginResponse);
   }
-  
+
 }
--- backend/src/main/java/com/a105/alub/api/service/UserService.java
+++ backend/src/main/java/com/a105/alub/api/service/UserService.java
@@ -5,13 +5,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import com.a105.alub.api.request.LoginReq;
 import com.a105.alub.api.response.LoginRes;
+import com.a105.alub.api.response.MyInfoRes;
 import com.a105.alub.domain.enums.Platform;
 
 public interface UserService extends UserDetailsService {
-  
+
   LoginRes login(LoginReq loginReq);
 
   UserDetails loadUserByUsername(String username, Platform platform)
       throws UsernameNotFoundException;
-  
+
+  MyInfoRes getMyInfo(Long userId);
 }
