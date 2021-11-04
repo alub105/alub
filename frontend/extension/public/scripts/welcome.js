2f647ad849edd316c3cbcb522a61ec9605556cee
choieunsong <greatgelly@gmail.com>
2021-11-04 10:04:09 +0900

    chore 로고 사이즈 변경

--- frontend/extension/public/scripts/welcome.js
+++ frontend/extension/public/scripts/welcome.js
@@ -1,7 +1,24 @@
-import axios from { axios };
+// const token = chrome.storage.sync.get("token", function (token) {
+//   console.log("token", token);
+// });
 
-const token = chrome.storage.sync.get("token", function (token) {
-  console.log("token", token);
-});
+// const base_url = "http://localhost:8080/api/";
 
-const base_url = "http://localhost:8080/api/";
+// $(document).ready(function () {
+//   // 기존 repo 목록 가져오기
+//   const get_repo_url = base_url + "repos";
+
+//   axios({
+//     method: "get",
+//     url: get_repo_url,
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//     },
+//   })
+//     .then((response) => {
+//       console.log(response);
+//     })
+//     .then((error) => {
+//       console.log(error);
+//     });
+// });
