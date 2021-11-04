efde55234f5bfe81a8d9905fff0e49c19bad75cb
yhpgKIM <greenboy94@naver.com>
2021-11-04 15:35:21 +0900

    feat: 원 클릭 커밋

---
+++ backend/src/main/java/com/a105/alub/api/request/CommitReq.java
@@ -0,0 +1,25 @@
+package com.a105.alub.api.request;
+
+import java.sql.Time;
+import com.a105.alub.domain.enums.CommitType;
+import com.a105.alub.domain.enums.Site;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class CommitReq {
+  String srcCode;
+  CommitType commit;
+  String fileName;
+  String sha;
+  String runningTime;
+  String runningMemory;
+  Time timer;
+  Site site;
+  String problemNum;
+  String problemName;
+  String language;
+}
---
+++ backend/src/main/java/com/a105/alub/api/response/GithubRepoContentRes.java
@@ -0,0 +1,12 @@
+package com.a105.alub.api.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class GithubRepoContentRes {
+  String name;
+}
