a7ec7ab2872c525c5d09b066d8e1edc5097767cf
ES_CHOI <greatgelly96@gmail.com>
2021-11-10 15:56:04 +0900

    feat: web 랜딩페이지 (#37)
    * fix: 웹사이트 배포 버전 수정
    
    * dd
    
    * 웹사이트 디자인
    
    * fix: 서버에서 익스텐션 작동하도록 수정
    
    * feat: web
    
    * feat: 랜딩페이지
    
    Co-authored-by: choieunsong <greatgelly@gmail.com>

--- frontend/web/src/component/LandingPage/LandingPage.js
+++ frontend/web/src/component/LandingPage/LandingPage.js
@@ -1,7 +1,13 @@
 import "./LandingPage.scoped.css";
 import background_ui from "../../static/image/background-ui.png";
-import ellipse from "../../static/image/Ellipse.svg";
+import ellipse_blue from "../../static/image/Ellipse-blue.svg";
+import ellipse_white from "../../static/image/Ellipse-white.svg";
 import logo from "../../static/image/logo.png";
+import meeting from "../../static/image/meeting.png";
+import baekjoon from "../../static/image/baekjoon.png";
+import github from "../../static/image/github.png";
+import programmers from "../../static/image/programmers.png";
+
 
 function LandingPage() {
   return (
@@ -37,16 +43,58 @@ function LandingPage() {
               </div>
             </div>
           </div>
-          <img src={ellipse} className="ellipse" />
+          <img src={ellipse_blue} className="ellipse" />
         </section>
         {/* -----x-- header -x-------- */}
         {/* -------- descript ---------- */}
-        <section className="section-1">
-          <div className="image-content">sdf</div>
-          <div className="descript">sdfsdfsdfsd </div>
+        <section className="section1">
+          <div className="image-box">
+            <img src={meeting} alt="meeting" className="meeting" />
+          </div>
+          <div className="flex-column text-box">
+            <h1>코딩 전용 온라인 스터디 채널을 만들어 보세요</h1>
+            <p>
+              ALUB 스터디는 그룹별 채널로 나뉩니다. 이메일을 통해 멤버를 초대하고, 스터디를
+              생성하고, 과제를 만들 수 있어요.
+            </p>
+          </div>
         </section>
+        <section className="section2">
+          <div className="flex-column text-box">
+            <h1>백준, 프로그래머스에 특화된 스터디</h1>
+            <p>
+              백준, 프로그래머스의 문제를 검색해 문제집을 만들 수 있어요. 그야말로 개발자를 위한
+              스터디 아닌가요?
+            </p>
+          </div>
+          <div className="image-box ">
+            <div>
+              <img src={programmers} alt="programmers" className="programmers" />
+            </div>
+            <div>
+              <img src={baekjoon} alt="baekjoon" className="baekjoon" />
+            </div>
+            <div>
+              <img src={github} alt="github" className="github" />
+            </div>
+          </div>
+        </section>
+        <section className="section3">
+          <div className="image-box " />
+          <div className="flex-column text-box">
+            <h1>백준, 프로그래머스에 특화된 스터디</h1>
+            <p>
+              백준, 프로그래머스의 문제를 검색해 문제집을 만들 수 있어요. 그야말로 개발자를 위한
+              스터디 아닌가요?
+            </p>
+          </div>
+        </section>
+        <img src={ellipse_white} className="ellipse-white" />
         {/* -----x-- descript -x-------- */}
       </main>
+      <footer>
+        <div className="footer">{/* <h1>footer</h1> */}</div>
+      </footer>
     </div>
   );
 }
