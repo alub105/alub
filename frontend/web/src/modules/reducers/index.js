79dd75eb9dba5189860b087f1197ec12f0fb39ec
choieunsong <greatgelly@gmail.com>
2021-11-13 17:54:03 +0900

    fix: web merge conflict 해결

--- frontend/web/src/modules/reducers/index.js
+++ frontend/web/src/modules/reducers/index.js
@@ -9,13 +9,7 @@ const persistConfig = {
   key: "root",
   storage,
 };
-<<<<<<< HEAD
-<<<<<<< develop
-=======
+
 const rootReducer = combineReducers({ user, study });
->>>>>>> local
-=======
-const rootReducer = combineReducers({ user });
->>>>>>> 7bb454397d2a39399ee31713e5a57bff7cbc44a4
 
 export default persistReducer(persistConfig, rootReducer);
