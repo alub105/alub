148499028e40017bb9b1c719afdc3e15463a0306
yonghun <greenboy94@naver.com>
2021-11-15 14:13:04 +0900

    feat: 파일 덮어쓰기 (#49)
    * feat: 덮어쓰기
    
    Closes S05P31A105-87

--- backend/src/main/java/com/a105/alub/api/service/UserService.java
+++ backend/src/main/java/com/a105/alub/api/service/UserService.java
@@ -6,10 +6,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import com.a105.alub.api.request.CommitReq;
 import com.a105.alub.api.request.ConfigsReq;
+import com.a105.alub.api.request.FileGetReq;
 import com.a105.alub.api.request.LoginReq;
 import com.a105.alub.api.request.RepoSetReq;
 import com.a105.alub.api.response.CommitRes;
 import com.a105.alub.api.response.ConfigsRes;
+import com.a105.alub.api.response.FileGetRes;
 import com.a105.alub.api.response.GithubRepoRes;
 import com.a105.alub.api.response.LoginRes;
 import com.a105.alub.api.response.MyInfoRes;
@@ -35,6 +37,8 @@ public interface UserService extends UserDetailsService {
   GithubRepoRes getGithubRepo(Long userId, String repoName);
 
   void setAlubRepo(Long userId, RepoSetReq repoSetReq);
+
+  public FileGetRes getFile(Long id, FileGetReq fileGetReq);
 
   public CommitRes commit(Long id, CommitReq commitReq);
 }
